70f9367167eeef199b3a815035826c14087a9f8f
cptroot <cptroot@gmail.com>
2013-07-29 21:36:38 -0700

    Moving Triangle

--- triangle.d
+++ triangle.d
@@ -1,17 +1,20 @@
 module triangle;
 
 import derelict.opengl3.gl3;
+import derelict.sdl2.sdl;
 
 import std.conv;
+import std.typecons;
+import std.math : sin, cos, PI;
 
 import component;
 import gl_shader;
 
-class Triangle : Drawable, Component {
+class Triangle : Drawable, Component, Updateable {
   const float vertexData[] = [
     0.0f,    0.5f, 0.0f, 1.0f,
     0.5f, -0.366f, 0.0f, 1.0f,
-    -0.5f, -0.366f, 0.0f, 1.0f,
+   -0.5f, -0.366f, 0.0f, 1.0f,
     1.0f,    0.0f, 0.0f, 1.0f,
     0.0f,    1.0f, 0.0f, 1.0f,
     0.0f,    0.0f, 1.0f, 1.0f,
@@ -19,6 +22,11 @@ class Triangle : Drawable, Component {
 
   uint positionBufferObject;
   uint shader;
+  uint offsetLocation;
+
+  Tuple!(float, float) offset;
+  float time = 0;
+  const float loopLength = 5f;
 
   @property float Depth() { return 1; };
 
@@ -36,9 +44,21 @@ class Triangle : Drawable, Component {
 
     shader = LoadProgram(["./triangle_shader.vert", "./triangle_shader.frag"], 
                          [GL_VERTEX_SHADER, GL_FRAGMENT_SHADER]);
+    
+    offsetLocation = glGetUniformLocation(shader, "offset");
   }
 
   @property int ID() { return 1; };
+
+  bool Update(SDL_Event[] events) {
+    time += .016f;
+    if (time >= loopLength) time -= loopLength;
+
+    offset[0] = .3 * sin(time / loopLength * 2 * PI);
+    offset[1] = .3 * cos(time / loopLength * 2 * PI);
+
+    return false;
+  }
 
   void Draw() {
     glUseProgram(shader);
@@ -46,6 +66,9 @@ class Triangle : Drawable, Component {
     glBindBuffer(GL_ARRAY_BUFFER, positionBufferObject);
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
+
+    glUniform2f(offsetLocation, offset[0], offset[1]);
+
     glVertexAttribPointer(0u, 4, GL_FLOAT, GL_FALSE, 0, cast(int*)0);
     glVertexAttribPointer(1u, 4, GL_FLOAT, GL_FALSE, 0, cast(int*)48);
 
